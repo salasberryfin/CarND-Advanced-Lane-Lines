f023733df2c6ea2f998c02dedf29db60d9e7a3bd
Carlos Salas <carlos.salas@keepler.io>
2019-12-09 15:25:53 +0100

    Improve formatting

--- img_operation.py
+++ img_operation.py
@@ -12,8 +12,6 @@ calibration_folder = "./camera_cal"
 calibration_image = "calibration3.jpg"
 calibration_path = os.path.join(calibration_folder, calibration_image)
 
-THRESH = (130, 255)
-
 
 class UndistImage:
     """ 
@@ -55,77 +53,54 @@ def identify_lanes(binary_warped):
     """
     histogram = np.sum(binary_warped[binary_warped.shape[0]//2:, :], axis=0)
     out_img = np.dstack((binary_warped, binary_warped, binary_warped))
-    # Find the peak of the left and right halves of the histogram
-    # These will be the starting point for the left and right lines
     midpoint = np.int(histogram.shape[0]//2)
     leftx_base = np.argmax(histogram[:midpoint])
     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-    # HYPERPARAMETERS
-    # Choose the number of sliding windows
     nwindows = 9
-    # Set the width of the windows +/- margin
     margin = 100
-    # Set minimum number of pixels found to recenter window
     minpix = 50
 
-    # Set height of windows - based on nwindows above and image shape
     window_height = np.int(binary_warped.shape[0]//nwindows)
-    # Identify the x and y positions of all nonzero pixels in the image
     nonzero = binary_warped.nonzero()
     nonzeroy = np.array(nonzero[0])
     nonzerox = np.array(nonzero[1])
-    # Current positions to be updated later for each window in nwindows
     leftx_current = leftx_base
     rightx_current = rightx_base
-
-    # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
     right_lane_inds = []
-
-    # Step through the windows one by one
     for window in range(nwindows):
-        # Identify window boundaries in x and y (and right and left)
         win_y_low = binary_warped.shape[0] - (window+1)*window_height
         win_y_high = binary_warped.shape[0] - window*window_height
-        # TO-DO: Find the four below boundaries of the window ###
         win_xleft_low = leftx_current - margin  # Update this
         win_xleft_high = leftx_current + margin  # Update this
         win_xright_low = rightx_current - margin  # Update this
         win_xright_high = rightx_current + margin  # Update this
         
-        # Draw the windows on the visualization image
         cv2.rectangle(out_img,(win_xleft_low,win_y_low),
         (win_xleft_high,win_y_high),(0,255,0), 2) 
         cv2.rectangle(out_img,(win_xright_low,win_y_low),
         (win_xright_high,win_y_high),(0,255,0), 2) 
         
-        ### TO-DO: Identify the nonzero pixels in x and y within the window ###
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
                           (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
         good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
                            (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
         
-        # Append these indices to the lists
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
         
-        ### TO-DO: If you found > minpix pixels, recenter next window ###
-        ### (`right` or `leftx_current`) on their mean position ###
         if len(good_left_inds) > minpix:
             leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
         if len(good_right_inds) > minpix:
             rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
 
-    # Concatenate the arrays of indices (previously was a list of lists of pixels)
     try:
         left_lane_inds = np.concatenate(left_lane_inds)
         right_lane_inds = np.concatenate(right_lane_inds)
     except ValueError:
-        # Avoids an error if the above is not implemented fully
         pass
 
-    # Extract left and right line pixel positions
     leftx = nonzerox[left_lane_inds]
     lefty = nonzeroy[left_lane_inds] 
     rightx = nonzerox[right_lane_inds]
@@ -138,14 +113,9 @@ def fit_polynomial(binary_warped):
     """
         Fit second order polynomial and generate values for plotting.
     """
-    # Find our lane pixels first
     leftx, lefty, rightx, righty, out_img = identify_lanes(binary_warped)
-
-    # TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
-
-    # Generate x and y values for plotting
     ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
     lane = Lane(ploty, left_fit, right_fit)
     try:
@@ -154,7 +124,6 @@ def fit_polynomial(binary_warped):
         lane.left_fitx = left_fitx
         lane.right_fitx = right_fitx
     except TypeError:
-        # Avoids an error if `left` and `right_fit` are still none or incorrect
         print('The function failed to fit a line!')
         left_fitx = 1*ploty**2 + 1*ploty
         right_fitx = 1*ploty**2 + 1*ploty
@@ -163,20 +132,8 @@ def fit_polynomial(binary_warped):
     # Colors in the left and right lane regions
     out_img[lefty, leftx] = [255, 0, 0]
     out_img[righty, rightx] = [0, 0, 255]
-
-    # Plots the left and right polynomials on the lane lines
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-
     out_img[lefty, leftx] = [255, 0, 0]
     out_img[righty, rightx] = [0, 0, 255]
-
-    # plt.imshow(out_img)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
-    # plt.show()
 
     return out_img, lane
 
@@ -261,6 +218,9 @@ def draw_output(image, warped, inv, lane):
 
 
 def insert_data(image, lane):
+    """
+        Write relevant data to final output.
+    """
     text_image = np.copy(image)
     font = cv2.FONT_HERSHEY_SIMPLEX
     curv_position = (10, 30)
@@ -314,6 +274,9 @@ def get_calibration():
 
 
 def hls_thresh(img, thresh):
+    """
+        Extract S channel of a HLS image.
+    """
     s = img[:, :, 2]
     binary_s = np.zeros_like(s)
     binary_s[(s > thresh[0]) & (s <= thresh[1])] = 1
@@ -322,12 +285,18 @@ def hls_thresh(img, thresh):
 
 
 def apply_hls(img):
+    """
+        Convert to HLS colorspace.
+    """
     hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
     
     return hls
 
 
 def gradient_magnitude(img, thresh, orient='x'):
+    """
+        Calculate the gradient for x and y.
+    """
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     if 'x' in orient:
         sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
@@ -342,6 +311,9 @@ def gradient_magnitude(img, thresh, orient='x'):
 
 
 def sobel(gray, sobel_kernel):
+    """
+        Calculate the sobel for x and y.
+    """
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     abs_sobelx = np.absolute(sobelx)
@@ -351,6 +323,9 @@ def sobel(gray, sobel_kernel):
 
 
 def dir_threshold(gray, abs_sobelx, abs_sobely, sobel_kernel=3, thresh=(0, np.pi/2)):
+    """
+        Calculate the direction of the gradient.
+    """
     direction = np.arctan2(abs_sobely, abs_sobelx)
     binary_output = np.zeros_like(direction)
     binary_output[(direction >= thresh[0]) & (direction <= thresh[1])] = 1
@@ -359,6 +334,9 @@ def dir_threshold(gray, abs_sobelx, abs_sobely, sobel_kernel=3, thresh=(0, np.pi
 
 
 def save_image_example(new_path, output_img):
+    """
+        Save image to file.
+    """
     img_name = new_path.split("/")[2]
     output_path = "output_images/new_undist/undist-{}".format(img_name)
     cv2.imwrite(output_path, output_img)
